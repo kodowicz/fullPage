eabe66506db9890c6755a893945e9e502479b84f
kodowicz <annakodowicz@gmail.com>
2018-09-26 15:35:16 +0200

    navigation fixed

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,26 @@
 function navigation () {
-  let navButton = document.querySelector('.nav-button');
-  let nav = document.querySelector('.navigation');
-  let navList = nav.querySelectorAll('.nav-list');
-  let navListItem = nav.querySelectorAll('.nav-list__item');
-  let navLinks = nav.querySelectorAll('.nav-link');
-  let buttonIconOpen = navButton.querySelector('.button-icon--open');
-  let buttonIconClose = navButton.querySelector('.button-icon--close');
+  const navButton = document.querySelector('.nav-button');
+  const buttonIconOpen = navButton.querySelector('.button-icon--open');
+  const buttonIconClose = navButton.querySelector('.button-icon--close');
 
+  const overlay = document.querySelector('.overlay');
+
+  const nav = document.querySelector('.navigation');
+  const navWrapper = nav.querySelector('.nav-wrapper');
+  const navList = nav.querySelector('.nav-list');
+  const navListItem = nav.querySelectorAll('.nav-list__item');
+  const navLinks = nav.querySelectorAll('.nav-link');
+
+
+  function initDelay() {
+    let delay = 0;
+    navListItem.forEach(li => {
+      if (li.style.display === 'none') return;
+      li.style.setProperty('transition-delay', `${delay}s`);
+      delay += 0.05;
+      return delay;
+    });
+  };
 
   function userInteraction() {
     navButton.addEventListener('keydown', handleKeydown);
@@ -20,36 +34,30 @@ function navigation () {
     };
   };
 
+
+
   function handleClick(event) {
     switch (navButton.getAttribute('aria-label')) {
       case ('Menu expanded'):
         navAnimation();
         removeAria();
         break;
+
       case ('Menu collapsed'):
         navAnimation();
         addAria();
         break;
+
       default:
         break;
     };
   };
 
-  function initDelay() {
-    let delay = 0;
-    navListItem.forEach(li => {
-      if (li.style.display === 'none') return;
-      li.style.setProperty('transition-delay', `${delay}s`);
-      delay += 0.05;
-      return delay;
-    });
-  };
-
-  ffunction navAnimation() {
+  function navAnimation() {
     switch (navButton.getAttribute('aria-label')) {
-    // close menu
       case ('Menu expanded'):
         navListItem.forEach(li => li.classList.remove('nav-list__item--expanded'));
+
         navListItem[navListItem.length-1].addEventListener('transitionend', function() {
           navWrapper.classList.remove('nav-wrapper--expanded');
           navWrapper.classList.add('nav-wrapper--collapsed');
@@ -57,15 +65,15 @@ function navigation () {
         }, {once: true});
         break;
 
-    // open menu
-      case ('Menu collapsed'):
-        overlay.classList.add('overlay--expanded');
-        navWrapper.classList.remove('nav-wrapper--collapsed');
-        navWrapper.classList.add('nav-wrapper--expanded');
-        navWrapper.addEventListener('transitionend', function() {
-          navListItem.forEach(li => li.classList.add('nav-list__item--expanded'));
-        }, {once: true});
-        break;
+        case ('Menu collapsed'):
+          overlay.classList.add('overlay--expanded');
+          navWrapper.classList.remove('nav-wrapper--collapsed');
+          navWrapper.classList.add('nav-wrapper--expanded');
+
+          navWrapper.addEventListener('transitionend', function() {
+            navListItem.forEach(li => li.classList.add('nav-list__item--expanded'));
+          }, {once: true});
+          break;
 
       default:
         break;
@@ -76,7 +84,7 @@ function navigation () {
     navButton.setAttribute('aria-label', 'Menu expanded');
     buttonIconOpen.setAttribute('aria-hidden', 'true');
     buttonIconClose.setAttribute('aria-hidden', 'false');
-    navList.forEach(list => list.setAttribute('aria-hidden', 'false'));
+    navWrapper.setAttribute('aria-hidden', 'false');
     navLinks.forEach(link => link.removeAttribute('tabIndex'));
   };
 
@@ -84,7 +92,7 @@ function navigation () {
     navButton.setAttribute('aria-label', 'Menu collapsed');
     buttonIconOpen.setAttribute('aria-hidden', 'false');
     buttonIconClose.setAttribute('aria-hidden', 'true');
-    navList.forEach(list => list.setAttribute('aria-hidden', 'true'));
+    navWrapper.setAttribute('aria-hidden', 'true');
     navLinks.forEach(link => link.setAttribute('tabIndex', '-1'));
   };
 
